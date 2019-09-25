845544f5950745555dbc0808ba8f53787862c7e6
nayankaba <nayan@stayntouch.com>
2019-09-25 14:54:36 -0400

    final changes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  # def setup
-  #
-  #   @base_title = "Ruby on Rails Tutorial Sample App"
-  #
-  # end
+  def setup
+
+    @base_title = "Ruby on Rails Tutorial Sample App"
+
+  end
 
   test "should get home" do
 
@@ -15,7 +15,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
 
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
 
   end
 
